f2b32e64a476dc49f6733b13c1b36c63fffac642
Benjamín Martínez Mateos <xaamin@outlook.com>
2016-09-15 14:53:01 -0500

    Added middleware to check token validity

--- src/Providers/Middleware/TokenValidation.php
+++ src/Providers/Middleware/TokenValidation.php
@@ -1,10 +1,19 @@
 <?php
-namespace Xaamin\TWT\Providers\Middleware;
+namespace Xaamin\JWT\Providers\Middleware;
 
 use Closure;
+use Xaamin\JWT\JWT;
+use Xaamin\JWT\Exceptions\JWTException;
 
 class TokenValidation
 {
+    protected $jwt;
+
+    public function __construct(JWT $jwt)
+    {
+        $this->jwt = $jwt;
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -14,6 +23,17 @@ class TokenValidation
      */
     public function handle($request, Closure $next)
     {
+        try {
+            $this->jwt->checkOrFail($request->header('Authorization'));
+        } catch (JWTException $e) {
+            $data = [
+                    'success' => false,
+                    'message' => 'Unauthorized'
+                ];
+
+            return response()->json($data, 401);
+        }
+
         return $next($request);
     }
 }
