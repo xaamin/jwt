93652622d65a810eb7b6a68a9c2378d65b10e5af
Benjamín Martínez Mateos <xaamin@outlook.com>
2023-04-21 09:31:15 -0600

    Added doc blocks comments

--- src/Facades/Jwt.php
+++ src/Facades/Jwt.php
@@ -5,6 +5,19 @@ namespace Xaamin\Jwt\Facades;
 use Xaamin\Jwt\Jwt as JwtBase;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Xaamin\Jwt\Token encode(array $claims)
+ * @method static \Xaamin\Jwt\Token decode(string $token)
+ * @method static \Xaamin\Jwt\Token refresh(string $token)
+ * @method static bool check(string $token)
+ * @method static bool checkOrFail(string $token)
+ * @method static \Xaamin\Jwt\Jwt with(string|null $secret, string $algorithm = 'HS512', array $keys = [])
+ * @method static \Xaamin\Jwt\Jwt setSigner(\Xaamin\Jwt\Contracts\Signer $issuer)
+ * @method static \Xaamin\Jwt\Jwt setIssuer(string $issuer)
+ * @method static \Xaamin\Jwt\Jwt setLeeway(int $seconds)
+ * @method static \Xaamin\Jwt\Jwt setTtl(int|null $minutes)
+ * @method static \Xaamin\Jwt\Jwt setRefreshTtl(int|null $minutes)
+ */
 class Jwt extends Facade
 {
     protected static function getFacadeAccessor()
--- src/Native/Jwt.php
+++ src/Native/Jwt.php
@@ -10,11 +10,15 @@ use Xaamin\Jwt\Jwt as JwtBase;
 /**
  * @method static \Xaamin\Jwt\Token encode(array $claims)
  * @method static \Xaamin\Jwt\Token decode(string $token)
+ * @method static \Xaamin\Jwt\Token refresh(string $token)
  * @method static bool check(string $token)
  * @method static bool checkOrFail(string $token)
- * @method static JwtBase setLeeway(int $seconds)
- * @method static JwtBase setTtl(int|null $minutes)
- * @method static JwtBase setRefreshTtl(int|null $minutes)
+ * @method static \Xaamin\Jwt\Jwt with(string|null $secret, string $algorithm = 'HS512', array $keys = [])
+ * @method static \Xaamin\Jwt\Jwt setSigner(\Xaamin\Jwt\Contracts\Signer $issuer)
+ * @method static \Xaamin\Jwt\Jwt setIssuer(string $issuer)
+ * @method static \Xaamin\Jwt\Jwt setLeeway(int $seconds)
+ * @method static \Xaamin\Jwt\Jwt setTtl(int|null $minutes)
+ * @method static \Xaamin\Jwt\Jwt setRefreshTtl(int|null $minutes)
  */
 class Jwt extends Facade
 {
