033b0ce4fb8babf6ea1a6d6e83fbb08cdbfe10f2
Benjamín Martínez Mateos <xaamin@outlook.com>
2023-04-21 09:24:48 -0600

    Moved service provider to the root

---
+++ src/JwtServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Xaamin\Jwt;
+
+use Xaamin\Jwt\Jwt;
+use Xaamin\Jwt\Factory;
+use Xaamin\Jwt\Signer\Native;
+use Illuminate\Support\ServiceProvider;
+
+class JwtServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (app()->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/jwt.php' => base_path('config/jwt.php'),
+            ], 'sanctum-config');
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(Jwt::class, function ($app) {
+            $passphrase = config('jwt.passphrase');
+            $algo = config('jwt.algorithm');
+            $keys = config('jwt.keys');
+
+            $factory = new Factory();
+            $signer = new Native($passphrase, $algo, $keys);
+
+            return new Jwt($signer, $factory);
+        });
+    }
+}
