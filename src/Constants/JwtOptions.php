913712325d8fbca9557db386b20f000341849c99
Benjamín Martínez Mateos <xaamin@outlook.com>
2023-04-26 07:13:00 -0600

    Renamed ttl to options to hold temp values

--- src/Constants/JwtOptions.php
+++ src/Constants/JwtOptions.php
@@ -2,9 +2,11 @@
 
 namespace Xaamin\Jwt\Constants;
 
-class JwtTtl
+class JwtOptions
 {
     public const LEEWAY = 0;
     public const TTL = 60;
     public const REFRESH_TTL = 20160;
+
+    public static $requiredClaims = ['iss', 'iat', 'exp', 'nbf', 'sub', 'jti'];
 }
